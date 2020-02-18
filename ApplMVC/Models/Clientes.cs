3571ff27049dbedf5f41d0adc226552ffc891f38
adrian <adriangaray396@gmail.com>
2020-02-17 22:25:55 -0300

    Se agregar validaciones(Data Annotations) y se empieza a trabajar con migraciones de base de datos

--- ApplMVC/Models/Clientes.cs
+++ ApplMVC/Models/Clientes.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Web;
 // se agrega el namespace
 using System.Data.Entity;
+// agrega el namespace: Nos va a permitir usar los atributos de validacion
+using System.ComponentModel.DataAnnotations;
 
 namespace ApplMVC.Models
 {
     public class Clientes
     {
         public int ID { get; set; }
+
+        [StringLength(60,MinimumLength = 3)]
         public string Nombre { get; set; }
+
+        [Display (Name = "Fecha de Alta")]
+        [DataType (DataType.Date)]
+        [DisplayFormat (DataFormatString ="{0:YYYY-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime FechaAlta { get; set; }
+
+        [Range(18, 75)]
         public int Edad { get; set; }
     }
 
